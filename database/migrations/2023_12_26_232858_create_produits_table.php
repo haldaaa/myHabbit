929b39f943c93e4cb488a6e828a90fa688de50b8
haldaaa <gasmi.fares@gmail.com>
2023-12-27 00:44:28 +0100

    Ajout migrations

--- database/migrations/2023_12_26_232858_create_produits_table.php
+++ database/migrations/2023_12_26_232858_create_produits_table.php
@@ -4,28 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClientsTable extends Migration
+class CreateProduitsTable extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-
-
     public function up()
     {
-        Schema::create('clients', function (Blueprint $table) {
+        Schema::create('produits', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->text('nomClient');
-            $table->text('ageClient');
-            $table->text('sexeClient');
-            $table->text('paysClient');
-            $table->text('villeClient');
-            $table->integer('commandeClient');
-             
-
+            $table->text('nomProduit');
+            $table->text('provenanceProduit');
+            $table->integer('totalVendu');
+            
         });
     }
 
@@ -36,6 +30,6 @@ class CreateClientsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('clients');
+        Schema::dropIfExists('produits');
     }
 }
